22e2105f11eb7ee8e2d4bdfdf5ea4577dd2dceed
jems6 <jmelend3@umd.edu>
2024-07-21 14:00:28 -0400

    name/tic to cik updates

--- lookup.py
+++ lookup.py
@@ -20,8 +20,21 @@ class SecEgdar:
         print(f'Loaded {len(self.comps)} companies, with {len(self.ticks)} tickers')
 
     def name_to_cik(self, name):
-        return self.comps.get(name)
-    def ticket_to_cik(self, ticket):
-        return self.ticks.get(ticket)
+        cik = self.comps.get(name)
+        if cik is None:
+            raise ValueError('Company not found')
+        for ticker, target in self.ticks.items():
+            if(target == cik):
+                return (name, ticker, cik)    
+        raise ValueError('Ticker not found')
+
+    def ticket_to_cik(self, ticker):
+        cik = self.ticks.get(ticker)
+        if cik is None:
+            raise ValueError('Ticker not found')
+        for name, target in self.comps.items():
+            if(target == cik):
+                return (name, ticker, cik)
+        raise ValueError('Company not found')
 s = SecEgdar('https://www.sec.gov/files/company_tickers.json')
 print(s.name_to_cik('Apple Inc.'))
